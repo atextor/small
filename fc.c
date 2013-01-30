6b3282a091e9fac56a4511b61b2e117e369a137c
Andreas Textor <textor.andreas@googlemail.com>
2013-01-30 13:40:28 +0100

    Lambda expressions in fc.c

--- fc.c
+++ fc.c
@@ -4,10 +4,17 @@
  * Andreas Textor <textor.andreas@googlemail.com>
  */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define LAMBDA(type, args, body) ({ type __lambda__ args body &__lambda__; })
+#define foreach(list, type, body) _foreach(l, LAMBDA(void, (type _), body))
+#define foldLeft(list, type, seed, body) _foldLeft(l, seed, LAMBDA(type, (type _1, type _2), body))
+#define forall(list, type, body) _forall(l, LAMBDA(bool, (type _), body))
+#define map(list, type, body) _map(l, LAMBDA(type, (type _), body))
 
 #define list_t(TYPE) struct Li_##TYPE*
 #define new_list(TYPE) new_list_##TYPE
@@ -25,20 +32,24 @@ struct Li_##TYPE {\
 	struct Li_##TYPE *tl;\
 	TYPE hd;\
 	bool isEmpty;\
-	TYPE (*foldLeft)(List_##TYPE l, TYPE seed, TYPE(f)(TYPE, TYPE));\
-	void (*foreach)(List_##TYPE l, void(f)(TYPE));\
-	bool (*forall)(List_##TYPE l, bool (f)(TYPE));\
-	List_##TYPE (*map)(List_##TYPE l, TYPE (f)(TYPE));\
+	char* typeName;\
+	TYPE (*_foldLeft)(List_##TYPE l, TYPE seed, TYPE(f)(TYPE, TYPE));\
+	void (*_foreach)(List_##TYPE l, void(f)(TYPE));\
+	bool (*_forall)(List_##TYPE l, bool (f)(TYPE));\
+	List_##TYPE (*_map)(List_##TYPE l, TYPE (f)(TYPE));\
 };\
 TYPE identity(TYPE v) { return v; }\
 List_##TYPE list_##TYPE##_nil() {\
 	List_##TYPE l = malloc(sizeof(struct Li_##TYPE));\
 	l->tl = NULL;\
 	l->isEmpty = true;\
-	l->foldLeft = list_##TYPE##_foldLeft;\
-	l->foreach = list_##TYPE##_foreach;\
-	l->forall = list_##TYPE##_forall;\
-	l->map = list_##TYPE##_map;\
+	l->_foldLeft = list_##TYPE##_foldLeft;\
+	l->_foreach = list_##TYPE##_foreach;\
+	l->_forall = list_##TYPE##_forall;\
+	l->_map = list_##TYPE##_map;\
+	l->typeName = malloc(strlen(#TYPE) + 1);\
+	memset(l->typeName, 0, strlen(#TYPE) + 1);\
+	snprintf(l->typeName, strlen(#TYPE) + 1, #TYPE);\
 	return l;\
 }\
 List_##TYPE list_##TYPE##_cons(TYPE v, List_##TYPE tl) {\
@@ -46,6 +57,7 @@ List_##TYPE list_##TYPE##_cons(TYPE v, List_##TYPE tl) {\
 	l->hd = v;\
 	l->tl = tl;\
 	l->isEmpty = false;\
+	l->typeName = tl->typeName;\
 	return l;\
 }\
 void list_##TYPE##_foreach(List_##TYPE l, void (f)(TYPE)) {\
@@ -98,24 +110,21 @@ List_##TYPE new_list_##TYPE(TYPE n, ...) {\
 
 USE_LIST(int)
 
-void printint(int i) { printf("%d ", i); }
-int timestwo(int i) { return i * 2; }
-bool isEven(int i) { return i % 2 == 0; }
-bool lessThan20(int i) { return i < 20; }
-int add(int a, int b) { return a + b; }
-
 int main(void) {
 	list_t(int) l = new_list(int)(1, 2, 3, 4, 5, 0);
-	l->foreach(l, printint);
+	l->foreach(l, int, { printf("%d ", _); });
 	putchar('\n');
-	printf("Sum %d\n", l->foldLeft(l, 0, add));
-	printf("All even? %d\n", l->forall(l, isEven));
-	printf("All <20? %d\n", l->forall(l, lessThan20));
+	printf("Sum %d\n", l->foldLeft(l, int, 0, { return _1 + _2; }));
 
-	list_t(int) l2 = l->map(l, timestwo);
-	list_t(int) l3 = l2->map(l2, identity);
-	l3->foreach(l3, printint);
+	printf("All even? %d\n", l->forall(l, int, { return _ % 2 == 0; }));
+	printf("All <20? %d\n", l->forall(l, int, { return _ < 20; }));
+
+	list_t(int) l2 = l->map(l, int, { return _ * 2; });
+	list_t(int) l3 = l->_map(l2, identity);
+	l3->foreach(l3, int, { printf("%d ", _); });
 	putchar('\n');
+
+	printf("Base type of l: %s\n", l->typeName);
 
 	return 0;
 }
